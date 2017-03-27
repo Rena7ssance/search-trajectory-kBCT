f4a48351bfc9d73441098f26c3bada8a1546a73a
Rena7ssance <roy7wt@gmail.com>
2017-03-27 23:25:20 +0800

    ts algorithm finish, set m equals 40/8, simplified rate

--- processor/test.py
+++ processor/test.py
@@ -4,19 +4,12 @@ from helper import *
 if __name__ == '__main__':
     # print Helper.lnglat2distance(121.342441, 31.263124, 121.341876, 31.263346)
     s = Simplifier()
-    print s.heading_direction([121.334382, 31.264038], [121.334338, 31.263517])
-    print s.heading_direction([121.334338, 31.263517], [121.334338, 31.262883])
+    # print s.heading_direction([121.334382, 31.264038], [121.334338, 31.263517])
+    # print s.heading_direction([121.334338, 31.263517], [121.334338, 31.262883])
     # Helper.heading_direction([0, 0], [-0.1, -0.9])
     # Helper.heading_direction([0, 0], [-1, 1])
     # Helper.heading_direction([0, 0], [-1.7, -1])
 
     t = Helper.file2points('data/test_data/test.txt')
-    s.ts_algorithm(t)
-    #
-    # for i in range(2-2,2+2):
-    #     print i
+    print s.ts_algorithm(t)
 
-    # r = []
-    # r.append({'a': 1, 'b': 2})
-    # r.append({'a': 2, 'b': 3})
-    # print r
